4c398c6ede246ff931d3784023f860760813d31d
Travis-Hurley <hurlean@ssfs.org>
2022-10-05 11:26:44 -0400

    egg

--- egg.py
+++ egg.py
@@ -1,9 +1,32 @@
 # Start writing your functions below this line
 
 
-def straight_Line():print("_______")
+def top_egg():
+    print("  _________")
+    print(" /         \\")
+    print("/           \\")
+
     # all of your function calls should go here. Remove the word "pass" before adding function calls.
-def next_line(): print(" /       \\")
+def bottom_egg():
+    print("\\           /")
+    print(" \\         /")
+    print("  ---------")
+def line():
+    print("-\"-\'-\"-\'-\"-\"-")
+def egg():
+    top_egg()
+    bottom_egg()
+def main():
+    egg()
+    line()
+
+    egg()
+    line()
+    bottom_egg()
+
+    top_egg()
+    line()
+    bottom_egg()
 
 if __name__ == '__main__':
     main()
