58913b996c128d1c7a64fc8c1bd9c9c826bafd50
Travis-Hurley <hurlean@ssfs.org>
2022-10-06 11:55:00 -0400

    addition stuff

--- addition.py
+++ addition.py
@@ -1,4 +1,17 @@
-# Write your addition function in the space below...
+# Write your addition function in the space below..
+
+number_1= input("First number >>")
+
+number_1=int(number_1)
+
+number_2=input('Second number >>')
+number_2=int(number_2)
+
+total = number_1+number_2
+def add_two(number_1,number_2):
+    print("The sum of", number_1,"and",number_2,"is", total)
+
+add_two(number_1,number_2)
 
 
 # Do not change anything below these lines
