3f44051b578e81394ae886ca7ca95ce3d88d72d3
Travis-Hurley <hurlean@ssfs.org>
2022-10-07 14:13:59 -0400

    Addition stuff

---
+++ Tests/Variables.py
@@ -0,0 +1,11 @@
+
+def areaofarectangle(legnth,hight):
+    area=legnth*hight
+    print(area)
+def main():
+    l=10
+    w=10
+    area=areaofarectangle(l,w)
+main()
+
+
--- addition.py
+++ addition.py
@@ -1,4 +1,9 @@
-# Write your addition function in the space below..
+#Travis Hurley
+#oct-7-22
+#Will add two numbers from user input
+
+'''
+
 
 number_1= input("First number >>")
 
@@ -8,10 +13,14 @@ number_2=input('Second number >>')
 number_2=int(number_2)
 
 total = number_1+number_2
-def add_two(number_1,number_2):
-    print("The sum of", number_1,"and",number_2,"is", total)
 
-add_two(number_1,number_2)
+'''
+
+def add_two(number_1,number_2):
+    print("The sum of", number_1,"and",number_2,"is", number_1+number_2)
+
+add_two(2,4)
+#add_two(number_1,number_2)
 
 
 # Do not change anything below these lines
