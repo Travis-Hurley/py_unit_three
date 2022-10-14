bf076cc70e8bdb605f7c257a798e955cf2291cb8
Travis-Hurley <hurlean@ssfs.org>
2022-10-14 14:15:36 -0400

    mmmm bad

--- assignment_three.py
+++ assignment_three.py
@@ -1,19 +1,48 @@
 #travis Hurley - Oct/13/2022 - calculate surface area of prism
-def main():
 
+def intro():
     print("This program will calculate the surface area of any prism!")
-    input("Enter anything to proceed>>")
+    input("Enter anything to proceed >>")
     """Assigns vaiables to numbers from user"""
+def reciving_info():
     l = int(input("What is the length of the prism? >>"))
     w = int(input("What is the width of the prism? >>"))
     h = int(input("What is the height of the prism? >>"))
     """calculates sides"""
-    rectangle_side1= (l*h)*2
-    rectangle_base = (h*w)*2
-    rectangle_side2 =(w*l)*2
-    """adds them for surface area"""
-    surface_area=rectangle_base+rectangle_side2+rectangle_side1
-    print("The surface area of your prism is",surface_area)
+    ll = l
+    ww = w
+    hh = h
+def side1(ll,hh):
+    rectangle_side1 = (ll*hh)*2
+    return rectangle_side1
+def side2(hh,ww):
+    rectangle_base = (hh*ww)*2
+    return rectangle_base
+def side3 (ww,ll):
+    rectangle_side2 =(ww*ll)*2
+    return rectangle_side2
+def combine():
+    surfaceArea=side1+side2+side3
+    return surfaceArea
+def closing():
+   print("The surface area of your prism is",combine())
+intro()
+reciving_info()
+side1()
+side2()
+side3()
+combine()
+closing()
+
+"""
+def main():
+    intro()
+    reciving_info()
+    side1()
+    side2()
+    side3()
+    closing()
+    closing()
 
 main()
-
+"""
