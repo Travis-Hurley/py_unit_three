425fc44b2c3676eedcce745e06ea62ae590f4fa7
Travis-Hurley <hurlean@ssfs.org>
2022-10-17 15:07:58 -0400

    check this - thanks :)

--- assignment_three.py
+++ assignment_three.py
@@ -1,33 +1,41 @@
-#travis Hurley - Oct/13/2022 - calculate surface area of prism
+#travis Hurley - Oct/17/2022 - calculate surface area of prism
 
 def intro():
     print("This program will calculate the surface area of any prism!")
     input("Enter anything to proceed >>")
-    """Assigns vaiables to numbers from user"""
-def reciving_info():
+    """Assigns variables to numbers from user"""
+def length(length):
     l = int(input("What is the length of the prism? >>"))
+    return l
+def width(width):
     w = int(input("What is the width of the prism? >>"))
+    return w
+def height(height):
     h = int(input("What is the height of the prism? >>"))
+    return h
     """calculates sides"""
-    ll = l
-    ww = w
-    hh = h
-def side1(ll,hh):
-    rectangle_side1 = (ll*hh)*2
+
+
+def side1(l,h):
+    rectangle_side1 = (length(l)*height(h))*2
     return rectangle_side1
-def side2(hh,ww):
-    rectangle_base = (hh*ww)*2
+def side2(h,w):
+    rectangle_base = (height(h)*width(w))*2
     return rectangle_base
-def side3 (ww,ll):
-    rectangle_side2 =(ww*ll)*2
+def side3 (w,l):
+    rectangle_side2 =(width(w)*length(l))*2
     return rectangle_side2
 def combine():
     surfaceArea=side1+side2+side3
     return surfaceArea
 def closing():
    print("The surface area of your prism is",combine())
+
+
 intro()
-reciving_info()
+length()
+height()
+width()
 side1()
 side2()
 side3()
